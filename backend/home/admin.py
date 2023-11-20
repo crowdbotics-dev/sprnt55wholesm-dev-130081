2f6706f42226cbaa62c592fde3781dd72af22441
crowdbotics-dev <engineering@crowdbotics.com>
2023-11-20 10:36:32 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Dfeg
+
+admin.site.register(Dfeg)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+
+class Dfeg(models.Model):
+    "Generated Model"
+    vaw1 = models.BigIntegerField()
+    vaw2 = models.CharField(
+        max_length=255,
+    )
+    vaw3 = models.TextField()

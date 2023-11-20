e0a9a81ae8facc8bc4ae06fc8cec4790dd431d5a
Crowdbotics <engineering@crowdbotics.com>
2023-11-20 07:17:42 +0000

    Update store.

--- store/sprntwholesmdevAPI/api.js
+++ store/sprntwholesmdevAPI/api.js
@@ -3,21 +3,25 @@ const sprntwholesmdevAPI = axios.create({
   baseURL: "https://sprnt55wholesm-dev-130081.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-function api_docs_schema_retrieve(payload) {
-  return sprntwholesmdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
-}
-function api_v1_connectors_spr55demosm_search_testing_retrieve(payload) {
-  return sprntwholesmdevAPI.get(
-    `/api/v1/connectors/spr55demosm/search/testing/`
-  )
-}
 function api_v1_login_create(payload) {
   return sprntwholesmdevAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
   return sprntwholesmdevAPI.post(`/api/v1/signup/`, payload)
+}
+function rest_auth_user_retrieve(payload) {
+  return sprntwholesmdevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return sprntwholesmdevAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return sprntwholesmdevAPI.patch(`/rest-auth/user/`, payload)
+}
+function api_docs_schema_retrieve(payload) {
+  return sprntwholesmdevAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
 }
 function rest_auth_login_create(payload) {
   return sprntwholesmdevAPI.post(`/rest-auth/login/`, payload)
@@ -25,17 +29,17 @@ function rest_auth_login_create(payload) {
 function rest_auth_logout_create(payload) {
   return sprntwholesmdevAPI.post(`/rest-auth/logout/`)
 }
-function rest_auth_password_change_create(payload) {
-  return sprntwholesmdevAPI.post(`/rest-auth/password/change/`, payload)
+function rest_auth_registration_create(payload) {
+  return sprntwholesmdevAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
   return sprntwholesmdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
+function rest_auth_password_change_create(payload) {
+  return sprntwholesmdevAPI.post(`/rest-auth/password/change/`, payload)
+}
 function rest_auth_password_reset_confirm_create(payload) {
   return sprntwholesmdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
-}
-function rest_auth_registration_create(payload) {
-  return sprntwholesmdevAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_resend_email_create(payload) {
   return sprntwholesmdevAPI.post(
@@ -49,29 +53,25 @@ function rest_auth_registration_verify_email_create(payload) {
     payload
   )
 }
-function rest_auth_user_retrieve(payload) {
-  return sprntwholesmdevAPI.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(payload) {
-  return sprntwholesmdevAPI.put(`/rest-auth/user/`, payload)
-}
-function rest_auth_user_partial_update(payload) {
-  return sprntwholesmdevAPI.patch(`/rest-auth/user/`, payload)
+function api_v1_connectors_spr55demosm_search_testing_retrieve(payload) {
+  return sprntwholesmdevAPI.get(
+    `/api/v1/connectors/spr55demosm/search/testing/`
+  )
 }
 export const apiService = {
-  api_docs_schema_retrieve,
-  api_v1_connectors_spr55demosm_search_testing_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  rest_auth_login_create,
-  rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
-  rest_auth_password_reset_confirm_create,
-  rest_auth_registration_create,
-  rest_auth_registration_resend_email_create,
-  rest_auth_registration_verify_email_create,
   rest_auth_user_retrieve,
   rest_auth_user_update,
-  rest_auth_user_partial_update
+  rest_auth_user_partial_update,
+  api_docs_schema_retrieve,
+  rest_auth_login_create,
+  rest_auth_logout_create,
+  rest_auth_registration_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_resend_email_create,
+  rest_auth_registration_verify_email_create,
+  api_v1_connectors_spr55demosm_search_testing_retrieve
 }
